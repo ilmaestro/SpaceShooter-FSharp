5125eaf2895af1dfdb0a1f5662fbe55c594631eb
Ryan Kilkenny <ilmaestro@gmail.com>
2015-02-24 08:17:24 -0800

    Player controller

--- UnityVS.SpaceShooter-FSharp.FSharp/GameComponents.fs
+++ UnityVS.SpaceShooter-FSharp.FSharp/GameComponents.fs
@@ -2,9 +2,38 @@
 open UnityEngine
 open System
 
-type Controller1()=
+type PlayerController() = 
     inherit MonoBehaviour()
 
-    member this.Start() =
-        Debug.Log("Component Started!")
+    [<SerializeField>]
+    [<DefaultValue>] val mutable speed : float32
 
+    let startEvt = Event<_>()
+    let fixedEvt = Event<_>()
+
+    member this.Start() =
+        startEvt.Trigger()
+
+    member this.FixedUpdate() =
+        fixedEvt.Trigger(Time.deltaTime)
+
+    member this.StartAsync() = Async.AwaitEvent startEvt.Publish
+    member this.FixedUpdateAsync() = Async.AwaitEvent fixedEvt.Publish
+
+    member this.Awake() =
+        let transform = this.GetComponent<Transform>()
+        let rigidbody = this.GetComponent<Rigidbody>()
+        let workflow = async {
+            do! this.StartAsync()
+            let rec gameloop() = async {
+                let! deltaTime = this.FixedUpdateAsync()
+                let moveHorizontal = Input.GetAxis("Horizontal")
+                let moveVertical = Input.GetAxis("Vertical")
+                let movement = new Vector3(moveHorizontal, 0.0f, moveVertical)
+
+                rigidbody.velocity <- (movement * this.speed * deltaTime)
+                return! gameloop()
+            }
+            return! gameloop()
+        }
+        workflow |> Async.StartImmediate |> ignore
